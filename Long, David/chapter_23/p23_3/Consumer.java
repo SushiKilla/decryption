38e982bea5f98d9b408a30bd71adfb20ddee8765
91040092 <91040092@HSS-101-18.westwindsor.k12.nj.us>
2014-10-31 14:49:56 -0400

    qqqqqqqqqqqq queue

--- Long, David/chapter_23/p23_3/Consumer.java
+++ Long, David/chapter_23/p23_3/Consumer.java
@@ -2,6 +2,14 @@ package p23_3;
 
 public class Consumer implements Runnable{
 
+	SynchronizedQueue<String> q;
+	int iter;
+	
+	public Consumer(SynchronizedQueue<String> queue, int iterations)
+	{
+		q = queue; iter = iterations;
+	}
+	
 	public void run() {
 		
 	}
--- Long, David/chapter_23/p23_3/Provider.java
+++ Long, David/chapter_23/p23_3/Provider.java
@@ -1,7 +1,15 @@
 package p23_3;
 
 public class Provider implements Runnable{
-
+	SynchronizedQueue<String> q;
+	String add;
+	int iter;
+	
+	public Provider(SynchronizedQueue<String> queue, String toAdd, int iterations)
+	{
+		q = queue; add = toAdd; iter = iterations;
+	}
+	
 	public void run() {
 		
 	}
