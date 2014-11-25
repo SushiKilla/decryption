5ec74e23a95dca6955f6d344c517229576c6e5c6
91040092 <91040092@HSS-101-18.westwindsor.k12.nj.us>
2014-11-25 13:11:26 -0500

    lol

--- Long, David/ChatProgram/server/Client.java
+++ Long, David/ChatProgram/server/Client.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		ClientThread clientThread = new ClientThread("127.0.0.1", 8888);
+		ClientThread clientThread = new ClientThread("10.5.100.90", 8888);
 		clientThread.start();
 	}
 }
