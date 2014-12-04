d3ef0890fbbcb0b860fa4b5416224288c4de5466
91040092 <91040092@HSS-101-18.westwindsor.k12.nj.us>
2014-12-04 14:15:47 -0500

    lol

--- Long, David/DistributedFactoring/factoring/Client.java
+++ Long, David/DistributedFactoring/factoring/Client.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 public class Client {
 	public static void main(String[] args) throws IOException {
 		String[] address = JOptionPane.showInputDialog("Enter the server's address (IP:port)").split(":");
-		
+		System.out.println();
 		ClientThread clientThread = new ClientThread(address[0], Integer.parseInt(address[1]));
 		clientThread.start();
 	}
