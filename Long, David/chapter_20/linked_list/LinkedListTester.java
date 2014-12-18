b1ff69938fb3a4a358b620fee3d7af62ee53027e
91040092 <91040092@HSS-101-18.westwindsor.k12.nj.us>
2014-12-18 12:03:16 -0500

    LinkedList Tester

--- Long, David/chapter_20/linked_list/LinkedListTester.java
+++ Long, David/chapter_20/linked_list/LinkedListTester.java
@@ -3,8 +3,28 @@ package linked_list;
 public class LinkedListTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyLinkedList<String> list = new MyLinkedList<String>();
+		list.add("A");
+		list.add("C");
+		list.add("D");
+		list.add("E");
+		list.add(1, "B");
+		
+		System.out.println(list.toString());
+		System.out.println(list.size());
+		
+		list.removeLast();
+		System.out.println(list.toString());
+		System.out.println(list.size());
+		
+		
+		list.remove(1);
+		System.out.println(list.toString());
+		System.out.println(list.size());
+		
+		list.clear();
+		System.out.println(list.size());
+		
 	}
 
 }
